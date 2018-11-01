84d073195bc276b8b9d7a15e8f9d686795bc78d8
KarimAK99 <KarimAbdelKader@outlook.com>
2018-11-01 11:22:22 -0700

    Initial template provided

---
+++ TestWordGraph/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordAnalysis;
+
+namespace TestWordGraph
+{
+    class Program
+    {
+        WordGraph wordGraph = new WordGraph();
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ WordGraph/WordGraph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordAnalysis
+{
+    public class WordGraph
+    {
+        private class Node
+        {
+            public string word = null;
+            public int count = 0;
+            public Dictionary<string, Edge> edges = null;
+        }
+
+        private class Edge
+        {
+            public Node fromNode = null;
+            public Node toNode = null;
+            public int weight = 0;
+        }
+
+        private Dictionary<string, Node> wordGraph = new Dictionary<string, Node>();
+
+        private string lastAddedWord = null;
+
+        public void AddWordToWordGraph(string word)
+        {
+        }
+
+        public void FinalizeGraph()
+        {
+        }
+
+        public List<List<string>> GetOrderedSequencesByLengthAndFrequency(int length, int frequency)
+        {
+            return null;
+        }
+
+        public List<List<string>> GetNonOrderedSequencesByLengthAndFrequency(int length, int frequency)
+        {
+            return null;
+        }
+    }
+}
